d7a720527df94b3f131234f94129aeed7bb3f4b7
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-07-18 16:39:19 +0530

    done

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import time
 import keyboard
 from GitAutomation.git_handler import git_push, git_pull
 
+# Define the default keyboard shortcuts
+DEFAULT_PUSH_SHORTCUT = 'Ctrl+L'
+DEFAULT_PULL_SHORTCUT = 'Ctrl+P'
+
 def handle_shortcut(event):
-    if event.name == 'l' and event.event_type == 'down':
+    # Get the configured keyboard shortcuts (or use the defaults)
+    push_shortcut = keyboard.get_hotkey_name(DEFAULT_PUSH_SHORTCUT)
+    pull_shortcut = keyboard.get_hotkey_name(DEFAULT_PULL_SHORTCUT)
+
+    # Check if the event matches the push shortcut
+    if event.name == push_shortcut and event.event_type == 'down':
         git_push()
-    elif event.name == 'p' and event.event_type == 'down':
+
+    # Check if the event matches the pull shortcut
+    elif event.name == pull_shortcut and event.event_type == 'down':
         git_pull()
 
 # Register keyboard shortcuts
